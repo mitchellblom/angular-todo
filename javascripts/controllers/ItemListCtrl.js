cf32e2babc742197629bb29b8190dd2e760d0975
Mitchell Blom <mitchellblom@gmail.com>
2017-05-16 20:04:09 -0500

    delete final item works

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -2,9 +2,22 @@ app.controller("ItemListCtrl", function($scope, ItemFactory) {		// own code goes
 	
 	$scope.items = [];
 
+	let getItems = () => {
 		ItemFactory.getItemList().then((itemz) => {		// now inside controller, will execute when controller loads
 			$scope.items = itemz;
 		}).catch((error) => {
 			console.log("get Error", error);
 		});
+	};
+
+	getItems();
+
+	$scope.deleteItem = (id) => {
+		console.log("here i am");
+		ItemFactory.deletz(id).then(() => {
+			getItems();
+		}).catch((error) => {
+			console.log("deleteItem error", error);
+		});
+	};
 });
--- javascripts/factories/ItemFactory.js
+++ javascripts/factories/ItemFactory.js
@@ -5,10 +5,12 @@ app.factory("ItemFactory", function($http, $q, FIREBASE_CONFIG) {
       $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
         .then((fbItems) => {
             var itemCollection = fbItems.data;
+            if(itemCollection.length !== null) {
             Object.keys(itemCollection).forEach((key) => {
                 itemCollection[key].id = key;
                 itemz.push(itemCollection[key]);
             });
+          };
             resolve(itemz);
         })
         .catch((error) => {
@@ -28,9 +30,22 @@ app.factory("ItemFactory", function($http, $q, FIREBASE_CONFIG) {
     });
   };
 
+  let deletz = (itemId) => {
+    return $q((resolve, reject) => {
+      $http.delete(`${FIREBASE_CONFIG.databaseURL}/items/${itemId}.json`)
+        .then((resultz) => {
+              resolve(resultz);
+          }).catch((error) => {
+              reject(error);
+          });
+    });
+  };
+
+
   return {
       getItemList: getItemList,
-      postNewItem: postNewItem
+      postNewItem: postNewItem,
+      deletz:deletz
   }; // now in factory objects callable by the given
 
 });
